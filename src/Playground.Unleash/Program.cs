7ee28d7a60fca77d1bb0540f9a5ca99f40d225b9
Espen Ekvang <espen.ekvang@moller.no>
2023-11-07 19:35:18 +0100

    chore: add simple unleash client

---
+++ src/Playground.Unleash/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Playground.Unleash;
+using Unleash;
+using Unleash.ClientFactory;
+
+var host = Host.CreateDefaultBuilder().ConfigureServices((context, services) =>
+    {
+        services.AddSingleton<IUnleash>(_ =>
+        {
+            var settings = new UnleashSettings()
+            {
+                AppName = "Playground.Unleash",
+                UnleashApi = new Uri("unleash-api-url"),
+                CustomHttpHeaders = new Dictionary<string, string>
+                {
+                    { "Authorization", "unleash-api-key" }
+                }
+            };
+
+            var unleashFactory = new UnleashClientFactory();
+
+            return unleashFactory.CreateClient(settings, synchronousInitialization: true);
+        });
+        services.AddTransient<UnleashClient>();
+    })
+    .Build();
+
+var unleashClient = host.Services.GetRequiredService<UnleashClient>();
+Console.WriteLine($"Toggles: {unleashClient.GetFeatureToggleCount()}");
---
+++ src/Playground.Unleash/UnleashClient.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+using Unleash;
+
+namespace Playground.Unleash;
+
+internal class UnleashClient
+{
+    private readonly ILogger<UnleashClient> _logger;
+    private readonly IUnleash _unleash;
+
+    public UnleashClient(ILogger<UnleashClient> logger, IUnleash unleash)
+    {
+        _logger = logger;
+        _unleash = unleash;
+    }
+
+    public int GetFeatureToggleCount()
+    {
+        return _unleash.FeatureToggles.Count;
+    }
+}
